9eec7d28b010f195ee83d551f95c4732e4876c0c
pezloco6586 <145697019+pezloco6586@users.noreply.github.com>
2024-03-20 17:06:51 +0000

    I added the score variable and code for that as an index at the end

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import random
 
 def flashcards_quiz():
     president_questions = []
+    correct = 0
     with open("flashcards (copy).json", "r") as f:
         flashcards = json.load(f)
     president_questions = list(flashcards.keys())
-    for _ in range(1):  
-        random.shuffle(president_questions)
-        for i in president_questions:
-            user_response = input(f"Who was the {i}th president of the United States? ")
-            if user_response == flashcards[i]:
-              print('correct')
-
+    for key in random.choices(president_questions, k=2-):
+      value = flashcards[key]
+      for i in president_questions:
+        user_response = input(f"Who was the {i}th president of the United States? ")
+        if user_response == value:
+          correct += 1
+        elif user_response != value:
+          correct += 0
+      score = f"{correct}/20"
+      print(score)
 
 flashcards_quiz()
